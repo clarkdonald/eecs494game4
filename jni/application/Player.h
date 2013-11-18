41e4b906d5d362b785cd26e8d89806f71148536b
Jason Xie <jasonx@umich.edu>
2013-11-18 02:56:33 -0500

    playerinterface

--- jni/application/Player.h
+++ jni/application/Player.h
@@ -11,12 +11,36 @@
 
 #include "Game_Object.h"
 
+#define MAX_HP 100.0f
+#define MAX_SPD 20.0f
+
 class Player : public Game_Object {
   public:
+	Player(const Zeni::Point2f &position_);
     virtual ~Player() = 0;
+
     virtual void render() = 0;
   
+	void handle_inputs(const Controls, const float);
+
+	void move_forward(const float);
+	void move_right(const float);
+	void turn_to_face(const float);
+
+	virtual bool std_attack();	  // sword, arrow, fireball
+	virtual void spc_skill() = 0; // special class-specific attack
+
+	void take_dmg(float);
+	void die();
+
+	void get_crystal();
+	void drop_crystal();
+
   private:
+	float facing;
+
+	float hp;
+	int n_crystals;
 };
 
 #endif /* PLAYER_H */
