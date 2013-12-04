2629a6567b3705e25be7c97d535624c7a7177383
Donald Clark <clarkds@umich.edu>
2013-12-03 19:57:27 -0500

    Rechanged the NPC bug

--- jni/application/Game_State.cpp
+++ jni/application/Game_State.cpp
@@ -641,7 +641,7 @@ void Game_State::load_map(const std::string &file_) {
     if (start_x < 0 || start_x >= dimension.width)
       error_handle("Invalid start x for player");
     Point2f pos(start_x*UNIT_LENGTH, start_y*UNIT_LENGTH);
-    team = (i % 2 ? BLUE : RED);
+    team = (i % 2 == 0 ? BLUE : RED);
     scores[team] = 0;
     player_wrappers.push_back(new Player_Wrapper(create_player("Mage", pos, i, team), i));
     player_wrappers.back()->player->kill();
